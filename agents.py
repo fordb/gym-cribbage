00e2c4a7532a50f420f23bc34121f1e3837e63eb
Ford Bohrmann <ford@fb.com>
2021-01-02 15:34:13 -0500

    rewrite montecarlo for true simulation without being able to anticipate the opponents hand

--- agents.py
+++ agents.py
@@ -1,9 +1,17 @@
-from gym_cribbage.envs.cribbage_env import CribbageEnv, evaluate_cards, Deck, Card
+from gym_cribbage.envs.cribbage_env import (
+    CribbageEnv,
+    evaluate_cards,
+    Deck,
+    Card,
+    Stack,
+    State
+)
 import numpy as np
 import random as rand
 import pandas as pd
 import logging
 import copy
+import datetime
 
 
 class Player(object):
@@ -139,83 +147,118 @@ class MonteCarlo(Greedy):
         else:
             self.p1 = Greedy()
         self.player = player
+
+    def score_hand(self, env, card1, card2, my_strategy, opp_strategy, player_num):
+        # simulates the score of a hand given the dealt hand, the cards to discard,
+        # the pegging strategy, and the opponent's strategy
+        reward_diff = 0
+        state = env.state
+        prev_phase = None
+        while True:
+            if state.phase == 0:
+                if state.hand_id == player_num:
+                    if card1 is not None:
+                        state, reward, done, debug = env.step(card1)
+                        card1 = None
+                    else:
+                        state, reward, done, debug = env.step(card2)
+                    reward_diff += reward
+                else:
+                    state, reward, done, debug = env.step(opp_strategy.play(env))
+                    reward_diff -= reward
+            elif state.phase == 1:
+                if state.hand_id == player_num:
+                    state, reward, done, debug = env.step(my_strategy.play(env))
+                    reward_diff += reward
+                else:
+                    state, reward, done, debug = env.step(opp_strategy.play(env))
+                    reward_diff -= reward
+            else:
+                if state.hand_id == player_num:
+                    state, reward, done, debug = env.step([])
+                    reward_diff += reward
+                else:
+                    state, reward, done, debug = env.step([])
+                    reward_diff -= reward
+
+            # end of a hand
+            if state.phase == 0 and prev_phase == 2:
+                return reward_diff
+            prev_phase = state.phase
+            # if game over, return reward
+            if done:
+                return reward_diff
+
+
+    def simulate_hand(self, player_num, hand, dealer, n_players):
+        # create a totally new environment with the same cards dealt
+        new_env = CribbageEnv(n_players=n_players, verbose=False, debug=False)
+        new_env.reset(dealer=dealer)
+
+        # reset the deck
+        new_env.deck = Deck()
+        # reset hands
+        new_env.hands = [Stack() for i in range(n_players)]
+        # add my sim hand to my hand
+        new_env.hands[player_num] = hand
+        # remove my sim hand from the deck
+        for c in hand:
+            new_env.deck = new_env.deck.remove(c)
+        # deal cards to other players
+        for i in range(n_players):
+            for j in range(new_env._cards_per_hand):
+                # only deal to other players
+                if i != player_num:
+                    new_env.hands[i].add_(new_env.deck.deal(player=i))
+
+        # reset the state
+        new_env.state = State(
+            new_env.hands[dealer],
+            dealer,
+            dealer,
+            0,
+            0,
+            []
+        )
+
+        return new_env
+
     
     def discard(self, env):
-
         # check if card to discard already calculated
         if not self.next_discard:
+            t1 = datetime.datetime.now()
             scores = np.zeros((6, 6))
-            hand = env.state.hand            
-            
+            hand = env.state.hand
+
             # loop through all 2 card combinations to discard
-            for n in range(self.trials):
-                for i in range(5):
-                    for j in range(i+1, 6):
-                        point_diff = 100
+            player_num = 0
+            hand = copy.deepcopy(env.hands[0])
+            dealer = copy.deepcopy(env.dealer)
+            n_players = copy.deepcopy(env.n_players)
 
-                        state = env.state
-                        
-                        # create copy of env
-                        _env = copy.deepcopy(env)
-                        _env.verbose = False
-                        _env.debug = False
-
-                        # play the rest of the hand
-                        card1 = _env.state.hand[i]
-                        card2 = _env.state.hand[j]
-
-                        prev_phase = None
-                        end_of_hand = False
-                        played = False
-                        while not end_of_hand:
-                            if _env.phase == 0:
-                                if state.hand_id == self.player:
-                                    if not played:
-                                        state, reward, done, debug = _env.step(card1)
-                                        played = True
-                                    else:
-                                        state, reward, done, debug = _env.step(card2)
-                                    point_diff += reward
-                                else:
-                                    # assume opponent plays greedily
-                                    state, reward, done, debug = _env.step(self.p2.play(_env))
-                                    point_diff -= reward
-                            elif _env.phase == 1:
-                                if state.hand_id == self.player:
-                                    # assume you peg greedily
-                                    state, reward, done, debug = _env.step(self.p2.play(_env))
-                                    point_diff += reward
-                                else:
-                                    # assume opponent pegs greedily
-                                    state, reward, done, debug = _env.step(self.p2.play(_env))
-                                    point_diff -= reward
-                            else:
-                                if state.hand_id == self.player:
-                                    state, reward, done, debug = _env.step([])
-                                    point_diff += reward
-                                else:
-                                    state, reward, done, debug = _env.step([])
-                                    point_diff -= reward
-
-                            # check if end of the hand
-                            if state.phase == 0 and prev_phase == 2:
-                                scores[i][j] += point_diff
-                                end_of_hand = True
-                        
-                            # check if game ended
-                            if done:
-                                scores[i][j] += point_diff
-                                end_of_hand = True
-                                
-                            prev_phase = state.phase
-                                
-
-            # calculate average score
-            scores /= self.trials
+            # get new environment
+            for i in range(5):
+                for j in range(i+1, 6):
+                    points = []
+                    card1 = hand[i]
+                    card2 = hand[j]
+                    for t in range(self.trials):
+                        new_env = self.simulate_hand(player_num, hand, dealer, n_players)
+                        # simulate
+                        my_strategy = Greedy()
+                        opp_strategy = Greedy()
+                        reward_diff = self.score_hand(new_env, card1, card2, my_strategy, opp_strategy, player_num)
+                        hand = copy.deepcopy(env.hands[0])
+                        points.append(reward_diff)
+                    scores[i][j] = np.mean(points)
 
             # find 2 best cards
+            # replace non-scores with large negative number
+            scores[scores == 0] = -1000
             discard1, discard2 = np.unravel_index(scores.argmax(), scores.shape)
             self.next_discard = hand[int(discard2)]
+            # print(datetime.datetime.now() - t1)
             return hand[int(discard1)]
         else:
             next_discard = self.next_discard
