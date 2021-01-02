17feb3bcd375d07bb505c8c30823d8b6ff8e9889
Ford Bohrmann <ford@fb.com>
2021-01-02 17:23:04 -0500

    clean up code for greedy. add verbose printing for MC

--- agents.py
+++ agents.py
@@ -90,24 +90,15 @@ class Greedy(HighCard):
     # play the card that gives the immediate most points
     def __init__(self):
         self.name = 'Greedy'
+        self.next_discard = None
 
     def discard(self, env):
         hand = self.get_hand(env)
         # track cards to discard
         top_score = -1
 
-        # if 5 cards left
-        if len(hand) == 5:
-            for i in range(5):
-                _hand = hand
-                _hand = _hand.remove(hand[i])
-                score = evaluate_cards(_hand)
-                if score > top_score:
-                    to_discard = hand[i]
-                    top_score = score
-
-        # if 6 cards left
-        if len(hand) == 6:
+        # if haven't found best set of cards
+        if self.next_discard is None:
             for i in range(5):
                 for j in range(i+1, 6):
                     _hand = hand
@@ -115,9 +106,14 @@ class Greedy(HighCard):
                     _hand = _hand.remove(hand[j])
                     score = evaluate_cards(_hand)
                     if score > top_score:
-                        to_discard = hand[i]
+                        to_discard = [hand[i], hand[j]]
                         top_score = score
-        return to_discard
+            self.next_discard = to_discard[1]
+            return to_discard[0]
+        else:
+            next_discard = self.next_discard
+            self.next_discard = None
+            return next_discard
 
     def peg(self, env):
         hand = self.get_hand(env)
@@ -138,17 +134,18 @@ class Greedy(HighCard):
 
 class MonteCarlo(Greedy):
     # simulate remaining hand and play card with the highest expected value
-    def __init__(self, player=0, trials=10):
-        self.name = 'Monte Carlo'
+    def __init__(self, player_num=0, trials=10, verbose=False):
+        self.name = 'Monte Carlo- {}'.format(trials)
         self.trials = trials
         self.next_discard = None
-        if player == 0:
+        if player_num == 0:
             self.p2 = Greedy()
         else:
             self.p1 = Greedy()
-        self.player = player
+        self.player_num = player_num
+        self.verbose = verbose
 
-    def score_hand(self, env, card1, card2, my_strategy, opp_strategy, player_num):
+    def score_hand(self, env, card1, card2, my_strategy, opp_strategy):
         # simulates the score of a hand given the dealt hand, the cards to discard,
         # the pegging strategy, and the opponent's strategy
         reward_diff = 0
@@ -156,7 +153,7 @@ class MonteCarlo(Greedy):
         prev_phase = None
         while True:
             if state.phase == 0:
-                if state.hand_id == player_num:
+                if state.hand_id == self.player_num:
                     if card1 is not None:
                         state, reward, done, debug = env.step(card1)
                         card1 = None
@@ -167,14 +164,14 @@ class MonteCarlo(Greedy):
                     state, reward, done, debug = env.step(opp_strategy.play(env))
                     reward_diff -= reward
             elif state.phase == 1:
-                if state.hand_id == player_num:
+                if state.hand_id == self.player_num:
                     state, reward, done, debug = env.step(my_strategy.play(env))
                     reward_diff += reward
                 else:
                     state, reward, done, debug = env.step(opp_strategy.play(env))
                     reward_diff -= reward
             else:
-                if state.hand_id == player_num:
+                if state.hand_id == self.player_num:
                     state, reward, done, debug = env.step([])
                     reward_diff += reward
                 else:
@@ -189,8 +186,7 @@ class MonteCarlo(Greedy):
             if done:
                 return reward_diff
 
-
-    def simulate_hand(self, player_num, hand, dealer, n_players):
+    def simulate_hand(self, hand, dealer, n_players):
         # create a totally new environment with the same cards dealt
         new_env = CribbageEnv(n_players=n_players, verbose=False, debug=False)
         new_env.reset(dealer=dealer)
@@ -200,7 +196,7 @@ class MonteCarlo(Greedy):
         # reset hands
         new_env.hands = [Stack() for i in range(n_players)]
         # add my sim hand to my hand
-        new_env.hands[player_num] = hand
+        new_env.hands[self.player_num] = hand
         # remove my sim hand from the deck
         for c in hand:
             new_env.deck = new_env.deck.remove(c)
@@ -208,7 +204,7 @@ class MonteCarlo(Greedy):
         for i in range(n_players):
             for j in range(new_env._cards_per_hand):
                 # only deal to other players
-                if i != player_num:
+                if i != self.player_num:
                     new_env.hands[i].add_(new_env.deck.deal(player=i))
 
         # reset the state
@@ -228,13 +224,18 @@ class MonteCarlo(Greedy):
         # check if card to discard already calculated
         if not self.next_discard:
             t1 = datetime.datetime.now()
+            lb = np.zeros((6, 6))
             scores = np.zeros((6, 6))
+            ub = np.zeros((6, 6))
             hand = env.state.hand
 
             # loop through all 2 card combinations to discard
-            player_num = 0
             hand = copy.deepcopy(env.hands[0])
             dealer = copy.deepcopy(env.dealer)
+            if self.verbose:
+                print("Hand: {}".format(hand))
+                print("Dealer? {}".format(dealer == self.player_num))
+
             n_players = copy.deepcopy(env.n_players)
 
             # get new environment
@@ -244,22 +245,32 @@ class MonteCarlo(Greedy):
                     card1 = hand[i]
                     card2 = hand[j]
                     for t in range(self.trials):
-                        new_env = self.simulate_hand(player_num, hand, dealer, n_players)
+                        new_env = self.simulate_hand(hand, dealer, n_players)
                         # simulate
                         my_strategy = Greedy()
                         opp_strategy = Greedy()
-                        reward_diff = self.score_hand(new_env, card1, card2, my_strategy, opp_strategy, player_num)
+                        reward_diff = self.score_hand(new_env, card1, card2, my_strategy, opp_strategy)
                         hand = copy.deepcopy(env.hands[0])
                         points.append(reward_diff)
                     scores[i][j] = np.mean(points)
+                    lb[i][j] = np.percentile(points, 5)
+                    ub[i][j] = np.percentile(points, 95)
 
             # find 2 best cards
             # replace non-scores with large negative number
             scores[scores == 0] = -1000
             discard1, discard2 = np.unravel_index(scores.argmax(), scores.shape)
-            self.next_discard = hand[int(discard2)]
-            # print(datetime.datetime.now() - t1)
-            return hand[int(discard1)]
+            discard1 = int(discard1)
+            discard2 = int(discard2)
+            if self.verbose:
+                print("Best play: {}, {}".format(hand[discard1], hand[discard2]))
+                print("Best play points: ({}, {}, {})".format(lb[discard1][discard2],
+                                                              scores[discard1][discard2],
+                                                              ub[discard1][discard2]))
+            self.next_discard = hand[discard2]
+            if self.verbose:
+                print("Time: {}".format(datetime.datetime.now() - t1))
+            return hand[discard1]
         else:
             next_discard = self.next_discard
             self.next_discard = None
